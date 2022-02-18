fe73b90292365744ef274e43b845388ee574a048
referenz <thomas.u.schueler@gmail.com>
2022-02-18 21:59:02 +0100

    Fördersumme "leer" statt "0" wird jetzt zur "0"

--- src/components/Projektliste.tsx
+++ src/components/Projektliste.tsx
@@ -6,7 +6,7 @@ import Infofelder from '../infofelder.json';
 import { Button } from 'react-bootstrap';
 */
 function formatCurrency(num: string): string {
-    const number = parseFloat(num);
+    const number = parseFloat(num) || 0;
     if (number === 0) return '';
     return number.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + ' Euro';
 }
